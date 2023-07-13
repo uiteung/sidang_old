ecd925f1aba32606b27c99fb1cb5975b0075025c
rofinafiin <rofinafiisr@gmail.com>
2023-07-13 11:43:57 +0700

    coba onclick

--- js/pdsidang.js
+++ js/pdsidang.js
@@ -1,15 +1,40 @@
 import { TokenLogin } from "./temp/config.js";
-import { PostSignUp, resetform, validateForm } from "./temp/table.js";
+import { resetform, validateForm } from "./temp/table.js";
 const form = document.querySelector('form');
 // "nomor": "4381/PUS.01.01/PDPBP"
   if (!TokenLogin) {
     window.location.replace("https://euis.ulbi.ac.id/")
   }
+  const formData = new FormData(form);
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
+  function PostSignUp() {
+    var myHeaders = new Headers();
+    myHeaders.append(TokenKey, TokenLogin);
+    myHeaders.append("Content-Type", "application/json");
+
+    var noskt = document.getElementById("nosrtskt").value;
+
     validateForm();
-    PostSignUp(formData);
-    resetform();
-  })
+    
+    var raw = JSON.stringify({
+      "nomor": noskt
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+  
+    fetch(urlPost, requestOptions)
+      .then(response => response.text())
+      .then(result => takeResponse(result))
+      .catch(error => console.log('error', error));
+  }
+  resetform();
+
+  // form.addEventListener('submit', (e) => {
+  //   e.preventDefault();
+
+  // })
