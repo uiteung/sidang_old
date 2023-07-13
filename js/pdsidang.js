8b6912e1511f0dc87a6a987c081533c23028ef92
rofinafiin <rofinafiisr@gmail.com>
2023-07-13 10:56:11 +0700

    direct cookie null

--- js/pdsidang.js
+++ js/pdsidang.js
@@ -1,6 +1,10 @@
+import { TokenLogin } from "./temp/config.js";
 import { PostSignUp, resetform } from "./temp/table.js";
 const form = document.querySelector('form');
 // "nomor": "4381/PUS.01.01/PDPBP"
+  if (!TokenLogin) {
+    window.location.replace("https://euis.ulbi.ac.id/")
+  }
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
