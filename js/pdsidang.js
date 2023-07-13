a1236b4b770f5cb4dd2c54730724409bc48af55c
rofinafiin <rofinafiisr@gmail.com>
2023-07-13 12:32:05 +0700

    no skt kosong

--- js/pdsidang.js
+++ js/pdsidang.js
@@ -8,31 +8,36 @@ const form = document.querySelector('form');
   const formData = new FormData(form);
 
 export function PostSignUp() {
-    console.log("udah di klik")
+    console.log("udah di klik");
     var myHeaders = new Headers();
     myHeaders.append(TokenKey, TokenLogin);
     myHeaders.append("Content-Type", "application/json");
 
     var noskt = document.getElementById("nosrtskt").value;
-
+    console.log(noskt);
     validateForm();
     
     var raw = JSON.stringify({
       "nomor": noskt
     });
     
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-  
-    fetch(urlPost, requestOptions)
-      .then(response => response.text())
-      .then(result => takeResponse(result))
-      .catch(error => console.log('error', error));
-  }
+    if (noskt === ""){
+      alert("cannot post")
+    }else{
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+    
+      fetch(urlPost, requestOptions)
+        .then(response => response.text())
+        .then(result => takeResponse(result))
+        .catch(error => console.log('error', error));
+    }
+    }
+
   resetform();
 
   // form.addEventListener('submit', (e) => {
