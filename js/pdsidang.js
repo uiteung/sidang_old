c16f2f2726cde57b4c9dee1448820c125b787178
rofinafiin <rofinafiisr@gmail.com>
2023-07-13 12:23:00 +0700

    aduh udah selesai kayaknya

--- js/pdsidang.js
+++ js/pdsidang.js
@@ -1,4 +1,4 @@
-import { TokenLogin } from "./temp/config.js";
+import { TokenLogin, TokenKey } from "./temp/config.js";
 import { takeResponse, resetform, validateForm } from "./temp/table.js";
 const form = document.querySelector('form');
 // "nomor": "4381/PUS.01.01/PDPBP"
